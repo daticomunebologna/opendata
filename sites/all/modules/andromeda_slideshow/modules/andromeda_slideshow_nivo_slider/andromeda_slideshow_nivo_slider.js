ff8f98465fcde066a6ac7c23ca96436c0262686d
Ninjinka <scolas@gmail.com>
2012-08-03 14:33:10 +0200

    pulizia completa

--- sites/all/modules/andromeda_slideshow/modules/andromeda_slideshow_nivo_slider/andromeda_slideshow_nivo_slider.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  Drupal.behaviors.andromeda_slideshow_nivo_slider = {
-    attach: function(context) {
-      $('.nivo-slider', context).nivoSlider({
-        directionNav: false
-      });
-    }
-  }
-})(jQuery);
--- sites/all/modules/andromeda_slideshow/modules/andromeda_slideshow_nivo_slider/nivo-slider.tpl.php
+++
@@ -1,7 +0,0 @@
-<div class="nivo-slider theme-pascal">
-  <?php if ($images) : ?>
-      <?php foreach ($images as $image) : ?>
-        <?php print $image->image_tag; ?>
-      <?php endforeach; ?>
-  <?php endif; ?>
-</div>
